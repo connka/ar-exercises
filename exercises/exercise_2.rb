64819a2c2979c8d2f652cce9da79aa03c8605042
Kate Connolly <katherine.d.connolly@gmail.com>
2018-12-05 22:37:34 +0000

    ex 1 and 2 complete

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,11 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+puts Store.first.name
+@store1 = Store.first
+puts "I've assigned it to an instance variable (#{@store1.name})"
+@store2 = Store.find_by(id:2)
+puts @store2.name
+@store1.name = "Calgary"
+puts @store1.name
+@store1.save
